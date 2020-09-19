a4dde3be53925baae69d35f19957387bef102101
Josewowgame2888 <mistertec7777777@gmail.com>
2020-09-19 23:54:14 +0000

    Fix: Net response invalid query

--- src/main/java/com/kibi/net/Responses.java
+++ src/main/java/com/kibi/net/Responses.java
@@ -9,7 +9,7 @@ public class Responses {
     public final static String REMOVE_OK = "REMOVE OK";
     public final static String CLEAR_OK = "CLEAR OK";
     public final static String INCORRECT_PASSWORD = "LOGIN FAILED";
-    public final static String INVALID_QUERY = "INVALID_QUERY";
+    public final static String INVALID_QUERY = "INVALID QUERY";
     public final static String FORCE_SHUTDOWN = "FORCE SHUTDOWN";
     public final static String IN_WHITELIST = "IN WHITELIST";
 }
