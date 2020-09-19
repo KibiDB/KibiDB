05a5fbd2b961f54f4f422d571d905cca738e4815
Josewowgame2888 <mistertec7777777@gmail.com>
2020-09-19 17:09:31 +0000

    CL IO alpha code hex 3xC09F

--- src/main/java/com/kibi/net/NetListenerResponse.java
+++ src/main/java/com/kibi/net/NetListenerResponse.java
@@ -39,55 +39,64 @@ public class NetListenerResponse extends Thread {
             String key = request.length >= 3 ? request[2] : null;
             String value = request.length >= 4 ? request[3] : null;
 
+            //responses
             switch (petition) {
                 case Requests.INSERT:
                     if (key == null || value == null) {
                         writer.writeUTF(Responses.INVALID_QUERY);
-                    } else {
-                        writer.writeUTF(Responses.INSERT_OK);
-                        database.set(key, value);
+
+                        break;
                     }
+
+                    writer.writeUTF(Responses.INSERT_OK);
+                    database.set(key, value);
                     break;
                 case Requests.GET:
                     if (key == null) {
                         writer.writeUTF(Responses.INVALID_QUERY);
-                    } else {
 
-                        if (!database.exists(key)) {
-                            writer.writeUTF(Responses.GET_NULL);
-                        } else {
-                            writer.writeUTF(database.getString(key));
-                        }
-
+                        break;
                     }
+
+                    if (!database.exists(key)) {
+                        writer.writeUTF(Responses.GET_NULL);
+
+                        break;
+                    }
+
+                    writer.writeUTF(database.getString(key));
                     break;
                 case Requests.SET:
                     if (key == null || value == null) {
                         writer.writeUTF(Responses.INVALID_QUERY);
-                    } else {
 
-                        if (!database.exists(key)) {
-                            writer.writeUTF(Responses.SET_NULL);
-                        } else {
-                            writer.writeUTF(Responses.SET_OK);
-                            database.set(key, value);
-                        }
-
+                        break;
                     }
+
+                    if (!database.exists(key)) {
+                        writer.writeUTF(Responses.SET_NULL);
+
+                        break;
+                    }
+
+                    writer.writeUTF(Responses.SET_OK);
+                    database.set(key, value);
                     break;
                 case Requests.REMOVE:
                     if (key == null) {
                         writer.writeUTF(Responses.INVALID_QUERY);
-                    } else {
 
-                        if (!database.exists(key)) {
-                            writer.writeUTF(Responses.REMOVE_NULL);
-                        } else {
-                            writer.writeUTF(Responses.REMOVE_OK);
-                            database.remove(key);
-                        }
-
+                        break;
                     }
+
+                    if (!database.exists(key)) {
+                        writer.writeUTF(Responses.REMOVE_NULL);
+
+                        break;
+                    }
+
+                    writer.writeUTF(Responses.REMOVE_OK);
+                    database.remove(key);
                     break;
                 case Requests.CLEAR:
                     writer.writeUTF(Responses.CLEAR_OK);
@@ -101,10 +110,11 @@ public class NetListenerResponse extends Thread {
                     break;
             }
 
-            net.channels.remove(client);
+            net.channels.remove(client);//remove because the connection ends
             client.close();
             writer.close();
         } catch (IOException e) {
+            net.channels.remove(client);//remove in case the connection fails
             Kibi.getLogger().warning(e.toString());
         }
     }
